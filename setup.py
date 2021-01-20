959a26499e12c40a92d0a62d437569b6281e2c18
multi-yt76 <72256091+multi-yt76@users.noreply.github.com>
2021-01-20 11:10:55 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+import re
+
+version = '1.0.0a'
+
+setup(name='toml-db',
+      author='multi-yt76',
+      url='https://github.com/multi-yt76/toml-db',
+      version=version,
+      packages=['tomldb'],
+      license='MIT',
+      description='A module which acts as a no-sql db, used to easily store and access data in toml format',
+      install_requires=['toml>=0.10.2'],
+      python_requires='>=3.5.3'
+)
